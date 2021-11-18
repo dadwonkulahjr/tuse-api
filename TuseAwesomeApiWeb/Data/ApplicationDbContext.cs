10441d2376fd83cbd451ff99246ae30824751db3
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-11-18 13:43:05 +0000

    Add the User class to the Models folder

--- TuseAwesomeApiWeb/Data/ApplicationDbContext.cs
+++ TuseAwesomeApiWeb/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace TuseAwesomeApiWeb.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
+        public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             :base(options)
         {
