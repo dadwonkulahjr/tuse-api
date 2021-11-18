27a18252ae919dd168d50ef9569cacb26721517d
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-11-18 14:27:56 +0000

    Added the User Repository Class

---
+++ TuseAwesomeApiWeb/Repo/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using TuseAwesomeApiWeb.Data;
+using TuseAwesomeApiWeb.Models;
+using TuseAwesomeApiWeb.Repo.IRepo;
+
+namespace TuseAwesomeApiWeb.Repo
+{
+    public class UserRepository : GenericRepo<User>, IUser
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserRepository(ApplicationDbContext applicationDbContext)
+            :base(applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public void UpdateUser(User userToUpdate)
+        {
+            var result = _applicationDbContext.Users.Attach(userToUpdate);
+            result.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            _applicationDbContext.SaveChanges();
+        }
+    }
+}
