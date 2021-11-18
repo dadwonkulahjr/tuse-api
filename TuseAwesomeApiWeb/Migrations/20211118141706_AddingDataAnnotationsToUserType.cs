749229d7d07ccddbf2c88b2d8f4259110007b998
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-11-18 14:19:14 +0000

    Added dataAnnotations to User Class

---
+++ TuseAwesomeApiWeb/Migrations/20211118141706_AddingDataAnnotationsToUserType.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TuseAwesomeApiWeb.Migrations
+{
+    public partial class AddingDataAnnotationsToUserType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
--- TuseAwesomeApiWeb/Models/User.cs
+++ TuseAwesomeApiWeb/Models/User.cs
@@ -1,11 +1,16 @@
-﻿namespace TuseAwesomeApiWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuseAwesomeApiWeb.Models
 {
     public class User
     {
         public int Id { get; set; }
 
+        [StringLength(50), Required]
         public string Username { get; set; }
+        [StringLength(50), Required]
         public string Password { get; set; }
+        [StringLength(50), EmailAddress(ErrorMessage ="Please enter a valid email address.")]
         public string Email { get; set; }
     }
 }
