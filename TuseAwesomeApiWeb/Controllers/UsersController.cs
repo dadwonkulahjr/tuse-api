22d66b26de2671b0b0e118cf37c8d38528b95942
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-11-18 16:56:07 +0000

    Added UserDtos

--- TuseAwesomeApiWeb/Controllers/UsersController.cs
+++ TuseAwesomeApiWeb/Controllers/UsersController.cs
@@ -27,9 +27,70 @@ namespace TuseAwesomeApiWeb.Controllers
                                     .ToList();
         }
 
+        [HttpPost]
+        public ActionResult Create([FromBody] CreateUserDto userDto)
+        {
+            if (!ModelState.IsValid) { return BadRequest(); }
 
 
+            if (userDto != null)
+            {
+                var newUser = new User()
+                {
+                    Username = userDto.Username,
+                    Password = userDto.Password,
+                    Email = userDto.Email
+                };
 
+                var result = _unitOfWork.UserRepository.AddAnItem(newUser);
+                _unitOfWork.Save();
+
+                return CreatedAtAction(nameof(GetUserById), new { id = result.Id }, result);
+            }
+
+
+            return BadRequest();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult GetUserById(int id)
+        {
+            var findUser = _unitOfWork.UserRepository.FilterItems(x => x.Id == id);
+
+            if (findUser != null)
+            {
+                return Ok(findUser);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, [FromBody] UpdateUserDto updateUserDto)
+        {
+            if (!ModelState.IsValid) { return BadRequest(); }
+
+            var findExistingUser = _unitOfWork.UserRepository.FilterItems(u => u.Id == id)
+                                                             .FirstOrDefault();
+
+            if(findExistingUser is null)
+            {
+                return NotFound();
+            }
+
+            var updateUser = new User()
+            {
+                Id = id,
+                Username = updateUserDto.Username,
+                Password = updateUserDto.Password,
+                Email = updateUserDto.Email
+            };
+
+            _unitOfWork.UserRepository.UpdateUser(updateUser);
+
+            return NoContent();
+        }
 
 
 
---
+++ TuseAwesomeApiWeb/Dtos/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuseAwesomeApiWeb.Dtos
+{
+    public class UpdateUserDto
+    {
+        [StringLength(50), Required]
+        public string Username { get; set; }
+        [StringLength(50), Required]
+        public string Password { get; set; }
+        [StringLength(50), EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+    }
+}
